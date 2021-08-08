5e0e360f2a696a8967973e1573b8b384ac70e66d
wilson <whilson03@gmail.com>
2021-08-08 18:40:51 +0100

    Initial commit

---
+++ app/src/main/java/com/olabode/wilson/cyclee/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.olabode.wilson.cyclee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.olabode.wilson.cyclee.ui.theme.CycleeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CycleeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CycleeTheme {
+        Greeting("Android")
+    }
+}
