80b990b94014aa8b4a6f42375dfe617490465fd3
Adeyoriju Olabode <whilson03@gmail.com>
2021-08-10 09:55:23 +0100

    migrated from Groovy dsl to kotlin dsl for dependency management

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Build.androidBuildTools)
+        classpath(Build.kotlinGradlePlugin)
+        classpath(Build.hiltAndroid)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
