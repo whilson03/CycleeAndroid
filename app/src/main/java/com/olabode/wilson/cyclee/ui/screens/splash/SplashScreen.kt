903073fd922fbcc2bca5e0b4707aaeb5059a0f76
Adeyoriju Olabode <whilson03@gmail.com>
2021-08-15 22:40:46 +0100

    added basic splash screen

---
+++ app/src/main/java/com/olabode/wilson/cyclee/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.olabode.wilson.cyclee.ui.screens.splash
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+
+private const val SplashWaitTime: Long = 2000
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        // This will always refer to the latest onTimeout function that
+        // SplashScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // Create an effect that matches the lifecycle of SplashScreen.
+        // If SplashScreen recomposes or onTimeout changes,
+        // the delay shouldn't start again.
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+        Text("CYCLEE")
+    }
+}
