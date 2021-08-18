5cc0e260b20b0ebea7904d2511bb0216eb029191
Adeyoriju Olabode <whilson03@gmail.com>
2021-08-18 22:13:59 +0100

    setup font and colors

--- app/src/main/java/com/olabode/wilson/cyclee/ui/theme/Color.kt
+++ app/src/main/java/com/olabode/wilson/cyclee/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.olabode.wilson.cyclee.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Orange600 = Color(0xFFF28B21)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Grey600 = Color(0xFF808080)
--- app/src/main/java/com/olabode/wilson/cyclee/ui/theme/Type.kt
+++ app/src/main/java/com/olabode/wilson/cyclee/ui/theme/Type.kt
@@ -2,27 +2,42 @@ package com.olabode.wilson.cyclee.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.olabode.wilson.cyclee.R
+
+private val Candara = FontFamily(
+    Font(R.font.candara_regular),
+    Font(R.font.candara_bold),
+    Font(R.font.candara_bold_italic),
+    Font(R.font.candara_italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Candara,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Candara,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Candara,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
-    )
-    */
+    ),
+
+    h5 = TextStyle(
+        fontFamily = Candara,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+
 )
