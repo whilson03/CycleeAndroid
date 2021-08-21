886fddd0d5950578cc90a7e1827f1194bdf42175
Adeyoriju Olabode <whilson03@gmail.com>
2021-08-21 18:55:55 +0100

    added bike doodle to OnBoardingScreen

--- app/src/main/java/com/olabode/wilson/cyclee/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/olabode/wilson/cyclee/ui/onboarding/OnboardingScreen.kt
@@ -1,17 +1,24 @@
 package com.olabode.wilson.cyclee.ui.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
+import com.olabode.wilson.cyclee.R
 import com.olabode.wilson.cyclee.data.OnBoardingData
 import com.olabode.wilson.cyclee.ui.theme.Orange600
 
@@ -33,23 +40,34 @@ fun OnBoardingScreenContent(
     val pageContents = remember { OnBoardingData.getOnBoardingContentList() }
     val pagerState = rememberPagerState(pageCount = pageContents.size)
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-
-        HorizontalPager(state = pagerState) { page ->
-            OnBoardingPageItem(data = pageContents[page])
-        }
-        HorizontalPagerIndicator(
-            pagerState = pagerState,
-            activeColor = Orange600,
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.bike_doodle),
+            contentDescription = "",
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
+                .fillMaxHeight(0.40f)
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            contentScale = ContentScale.Crop
         )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
-        OnBoardingNavigation {
-            openAuthScreen()
+            HorizontalPager(state = pagerState) { page ->
+                OnBoardingPageItem(data = pageContents[page])
+            }
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                activeColor = Orange600,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            OnBoardingNavigation {
+                openAuthScreen()
+            }
         }
     }
 }
